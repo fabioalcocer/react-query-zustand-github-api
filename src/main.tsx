f4d2868bb33ab76c54130cd5966df6c7678a5f77
fabio alcocer <fabioalcocersejas17@gmail.com>
2023-02-28 22:27:01 -0400

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { QueryClientProvider } from '@tanstack/react-query'
+import './index.css'
+import queryClient from './queryClient'
+
+ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+)
